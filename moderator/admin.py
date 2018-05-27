ba0d9907906273a12719c03edc518ccd93b162a8
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2018-05-27 16:20:05 -0400

    Added models to django admin

--- moderator/admin.py
+++ moderator/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Image, ModeratedImage, Moderator, ModerationLog
 
-# Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+  list_display = (
+    'url',
+    'timestamp'
+  )
+
+
+@admin.register(Moderator)
+class ModeratorAdmin(admin.ModelAdmin):
+  list_display = (
+    'name',
+  )
+
+
+@admin.register(ModeratedImage)
+class ModeratedImageAdmin(admin.ModelAdmin):
+  list_display = (
+    'image',
+    'moderator',
+    'decision',
+    'timestamp'
+  )
+  raw_id_fields = (
+    'image',
+    'moderator',
+  )
+
+
+@admin.register(ModerationLog)
+class ModerationLogAdmin(admin.ModelAdmin):
+  list_display = (
+    'image',
+    'moderator',
+    'decision',
+    'timestamp'
+  )
+  raw_id_fields = (
+    'image',
+    'moderator',
+  )
