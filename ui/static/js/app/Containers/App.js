95528a82d085f495f6ffb06d3f625ec05813457d
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2018-05-28 23:28:31 -0400

    Navigation between dashboard/moderation

--- ui/static/js/app/Containers/App.js
+++ ui/static/js/app/Containers/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import {
-    TopAppBar,
-    TopAppBarRow,
-    TopAppBarSection,
-    TopAppBarTitle
-  } from 'rmwc/TopAppBar';
-import { Select } from 'rmwc/Select';
 
-
+import NavBar from './NavBar';
 import ModeratedImages from '../Components/ModeratedImages';
 import Moderator from '../Components/Moderator';
 
@@ -17,7 +10,7 @@ export default class App extends React.Component {
     componentWillMount() {
         let moderators = this.props.config && this.props.config.moderators || [];
         this.state = {
-            moderator: moderators.length > 0 ? moderators[0] : null
+            moderator: moderators.length > 0 ? moderators[0] : null,
         }
     }
 
@@ -30,11 +23,11 @@ export default class App extends React.Component {
                 <NavBar
                     moderator={this.state.moderator}
                     moderators={moderators}
-                    onSetModerator={(m) => this.setState({moderator: m})}/>
+                    onSetModerator={(m) => this.setState({moderator: m})} />
             </div>
-        )
+        );
     }
-}
+};
 
 class Body extends React.Component {
     render() {
@@ -51,41 +44,6 @@ class Body extends React.Component {
                     </Switch>
                 </BrowserRouter>
             </main>
-        );
-    }
-};
-
-class NavBar extends React.Component {
-
-    renderModeratorChoice() {
-        let moderators = this.props.moderators || [];
-        if (moderators.length == 0) {
-            return (<span>No moderators</span>);
-        } else {
-            return (
-                <Select box
-                    value={this.props.moderator}
-                    onChange={evt => this.props.onSetModerator(evt.target.value)}
-                    label="Moderator"
-                    placeholder=""
-                    options={moderators.map((i) => {return {label: i, value: i}; } )}
-                />
-            );
-        }
-    }
-
-    render() {
-        return (
-            <TopAppBar>
-                <TopAppBarRow>
-                    <TopAppBarSection alignStart>
-                        <TopAppBarTitle>Image Moderator</TopAppBarTitle>
-                    </TopAppBarSection>
-                    <TopAppBarSection alignEnd>
-                        {this.renderModeratorChoice()}
-                    </TopAppBarSection>
-                </TopAppBarRow>
-            </TopAppBar>
         );
     }
 };
---
+++ ui/static/js/app/Containers/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  TopAppBar,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarNavigationIcon,
+  TopAppBarActionItem,
+  TopAppBarTitle
+} from 'rmwc/TopAppBar';
+import { Select } from 'rmwc/Select';
+import { Button } from 'rmwc/Button';
+
+export default class NavBar extends React.Component {
+
+  renderModeratorChoice() {
+      let moderators = this.props.moderators || [];
+      if (moderators.length == 0) {
+          return (<span>No moderators</span>);
+      } else {
+          return (
+              <Select box
+                  value={this.props.moderator}
+                  onChange={evt => this.props.onSetModerator(evt.target.value)}
+                  label="Moderator"
+                  placeholder=""
+                  options={moderators.map((i) => {return {label: i, value: i}; } )}
+              />
+          );
+      }
+  }
+
+  navigateTo(page) {
+    window.location.pathname = '/ui/' + page
+  }
+
+  render() {
+      return (
+          <TopAppBar>
+              <TopAppBarRow>
+                  <TopAppBarSection alignStart>
+                      <TopAppBarTitle>Image Moderator</TopAppBarTitle>
+                      <Button style={{marginLeft: '15px'}} raised onClick={() => this.navigateTo('')} theme="secondary-bg on-secondary">
+                        Dashboard
+                      </Button>
+                      <Button style={{marginLeft: '15px'}} raised onClick={() => this.navigateTo('moderation')} theme="secondary-bg on-secondary">
+                        Moderation
+                      </Button>
+                  </TopAppBarSection>
+                  <TopAppBarSection alignEnd>
+                      {this.renderModeratorChoice()}
+                  </TopAppBarSection>
+              </TopAppBarRow>
+          </TopAppBar>
+      );
+  }
+};
