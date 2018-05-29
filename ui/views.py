ca5122d79ccdddb6b1da9dc4ab77cd83eb0a5ee9
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2018-05-29 00:59:56 -0400

    Actually returning moderators as context for front end

--- ui/views.py
+++ ui/views.py
@@ -3,11 +3,13 @@ ui.views
 '''
 
 from django.shortcuts import render
+from moderator.models import Moderator
 
 
 def ui(request):
     """
     Serves react app for the image moderation UI.
     """
-    context = { 'props': {'moderators': [ 'dog', 'mat' ] } }
+    moderators = list(map( lambda x: x.name, Moderator.objects.all() ))
+    context = { 'props': {'moderators': moderators } }
     return render(request, 'ui/base.html', context=context)
