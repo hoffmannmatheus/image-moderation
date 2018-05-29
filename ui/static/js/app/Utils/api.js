13477ba2c7400f74bbf221f06b0ca9c77fbfc6ac
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2018-05-29 00:31:17 -0400

    Fix for csrf token

--- ui/static/js/app/Utils/api.js
+++ ui/static/js/app/Utils/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 export default axios.create({
   baseURL: `http://${window.location.host}/api/`
 });
